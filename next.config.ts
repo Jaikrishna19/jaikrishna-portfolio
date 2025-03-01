037a5e504bb27011403a7328136e2c0b77999471
Jaikrishna19 <jaikrishnajaisankar2005@gmail.com>
2025-03-01 14:43:59 +0530

    nextConfig changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-};
+  output: 'export',
+  basePath: '/jaikrishna-portfolio',
+  assetPrefix: '/jaikrishna-portfolio/',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+}
 
 export default nextConfig;
